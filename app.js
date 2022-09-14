57f99e08d0a9636ec6904af4aceca567fc64cf78
unknown <mustafamjaber@yahoo.com>
2022-09-14 10:59:44 +0200

    deploy the app in reder

--- app.js
+++ app.js
@@ -15,6 +15,16 @@ app.use("/api/v1/realEstate", realEstateRouter);
 
 //fetchRalEstate();
 
+//* Serve static assets in production, must be at this location of this file
+if (process.env.NODE_ENV === "production") {
+  //*Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
 const port = process.env.PORT || 4200;
 
 //*start the server
